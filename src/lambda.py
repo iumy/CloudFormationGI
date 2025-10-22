d5f15721dece87b41613ff6b7442ba13208575c2
Gianluca Iumiento <giumiento@prostrategy.ie>
2025-10-22 15:43:08 +0100

    reformatted with black

--- src/lambda.py
+++ src/lambda.py
@@ -1,10 +1,10 @@
 import json
+
+
 def lambda_handler(event, context):
-    ip_address = event['requestContext']['identity']['sourceIp']
+    ip_address = event["requestContext"]["identity"]["sourceIp"]
     return {
-        'body': json.dumps({"ip_address": ip_address}),
-        'headers': {
-            'Content-Type': 'application/json'
-        },
-        'statusCode': 200
+        "body": json.dumps({"ip_address": ip_address}),
+        "headers": {"Content-Type": "application/json"},
+        "statusCode": 200,
     }
